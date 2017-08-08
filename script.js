1b5485dd7983180381e34d2a8202b56d0a60f451
Benjy Wiener <info@BenjyWiener.com>
2017-08-08 13:25:58 +0300

    Included angular.js, angular-animate.js; Created scripts.js, style.scss

---
+++ script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Benjy on 8/8/17.
+ */
+
+var app = angular.module("app", [])
+
+.controller("AppCtrl", function ($scope) {
+
+})
+
+.controller("AnimationCtrl", function ($scope) {
+
+})
